1b8bf9b8b11d6332a189dd0ad697f86825e104b4
Todd Vanderlin <tvanderlin@bo-m-tvanderlin.ideo.com>
2014-03-17 17:20:50 -0400

    IOS Utils fix

--- AppUtils.h
+++ AppUtils.h
@@ -93,7 +93,18 @@ static void printAllFonts() {
 @end
 
 
-
+#pragma mark --- UITextView ---
+@interface UITextView (UITextViewExtension)
+-(void)actLikeTextLabel;
+@end
+@implementation UITextView (UITextViewExtension)
+-(void)actLikeTextLabel {
+    self.selectable = NO;
+    self.scrollEnabled = NO;
+    self.showsHorizontalScrollIndicator = NO;
+    self.showsVerticalScrollIndicator = NO;
+}
+@end
 
 
 #pragma mark --- UIView ---
@@ -113,6 +124,9 @@ static void printAllFonts() {
 -(CALayer*)addRightBorder:(UIColor*)color;
 -(CALayer*)addLeftBorder:(UIColor*)color;
 -(void)removeLayerName:(NSString*)name;
+-(void)setWidth:(CGFloat)width;
+-(void)setHeight:(CGFloat)height;
+
 -(void)setWidth:(CGFloat)width andHeight:(CGFloat)height;
 -(void)fadeToAlpha:(CGFloat)alpha speed:(CGFloat)speed delay:(CGFloat)delay onComplete:(BasicBlock)completeBlock;
 
@@ -213,6 +227,13 @@ static void printAllFonts() {
     rec.size = CGSizeMake(width, height);
     self.frame = rec;
 }
+-(void)setWidth:(CGFloat)width {
+    [self setWidth:width andHeight:self.frame.size.height];
+}
+-(void)setHeight:(CGFloat)height {
+    [self setWidth:self.frame.size.width andHeight:height];
+}
+
 @end
 
 
@@ -340,7 +361,7 @@ static void printAllFonts() {
     return [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:alpha];
 }
 + (UIColor *)colorWithColor:(UIColor*)color alpha:(CGFloat)alpha {
-    double r,g,b,a;
+    CGFloat r,g,b,a;
     [color getRed:&r green:&g blue:&b alpha:&a];
     return [UIColor colorWithRed:r green:g blue:b alpha:alpha];
 }
@@ -453,7 +474,140 @@ static void printAllFonts() {
 
 
 
+#pragma mark --- NSString ---
+//--------------------------------------------------------------
+// NSAttributedString Extensions
+//--------------------------------------------------------------
 
+
+/*@implementation NSAttributedString (Geometrics)
+
+#pragma mark * Measure Attributed String
+
+- (NSSize)sizeForWidth:(float)width
+				height:(float)height {
+	NSSize answer = NSZeroSize ;
+    if ([self length] > 0) {
+		// Checking for empty string is necessary since Layout Manager will give the nominal
+		// height of one line if length is 0.  Our API specifies 0.0 for an empty string.
+		NSSize size = NSMakeSize(width, height) ;
+		NSTextContainer *textContainer = [[NSTextContainer alloc] initWithContainerSize:size] ;
+		NSTextStorage *textStorage = [[NSTextStorage alloc] initWithAttributedString:self] ;
+		NSLayoutManager *layoutManager = [[NSLayoutManager alloc] init] ;
+		[layoutManager addTextContainer:textContainer] ;
+		[textStorage addLayoutManager:layoutManager] ;
+		[layoutManager setHyphenationFactor:0.0] ;
+		if (gNSStringGeometricsTypesetterBehavior != NSTypesetterLatestBehavior) {
+			[layoutManager setTypesetterBehavior:gNSStringGeometricsTypesetterBehavior] ;
+		}
+		// NSLayoutManager is lazy, so we need the following kludge to force layout:
+		[layoutManager glyphRangeForTextContainer:textContainer] ;
+        
+		answer = [layoutManager usedRectForTextContainer:textContainer].size ;
+#if NO_ARC
+		[textStorage release] ;
+		[textContainer release] ;
+#endif
+		// Adjust if there is extra height for the cursor
+		NSSize extraLineSize = [layoutManager extraLineFragmentRect].size ;
+		if (extraLineSize.height > 0) {
+			answer.height -= extraLineSize.height ;
+		}
+		
+#if NO_ARC
+		[layoutManager release] ;
+#endif
+		// In case we changed it above, set typesetterBehavior back
+		// to the default value.
+		gNSStringGeometricsTypesetterBehavior = NSTypesetterLatestBehavior ;
+	}
+    
+	return answer ;
+}
+
+- (float)heightForWidth:(float)width {
+	return [self sizeForWidth:width
+					   height:FLT_MAX].height ;
+}
+
+- (float)widthForHeight:(float)height {
+	return [self sizeForWidth:FLT_MAX
+					   height:height].width ;
+}
+
+@end
+
+
+@implementation NSString (Geometrics)
+
+#pragma mark * Given String with Attributes
+
+- (NSSize)sizeForWidth:(float)width
+				height:(float)height
+			attributes:(NSDictionary*)attributes {
+	NSSize answer ;
+    
+	NSAttributedString *astr = [[NSAttributedString alloc] initWithString:self
+															   attributes:attributes] ;
+	answer = [astr sizeForWidth:width
+						 height:height] ;
+#if NO_ARC
+	[astr release] ;
+#endif
+    
+	return answer ;
+}
+
+- (float)heightForWidth:(float)width
+			 attributes:(NSDictionary*)attributes {
+	return [self sizeForWidth:width
+					   height:FLT_MAX
+				   attributes:attributes].height ;
+}
+
+- (float)widthForHeight:(float)height
+			 attributes:(NSDictionary*)attributes {
+	return [self sizeForWidth:FLT_MAX
+					   height:height
+				   attributes:attributes].width ;
+}
+
+#pragma mark * Given String with Font
+
+- (NSSize)sizeForWidth:(float)width
+				height:(float)height
+				  font:(NSFont*)font {
+	NSSize answer = NSZeroSize ;
+    
+	if (font == nil) {
+		NSLog(@"[%@ %@]: Internal Error 561-3810: Nil font", [self class], NSStringFromSelector(_cmd)) ;
+	}
+	else {
+		NSDictionary* attributes = [NSDictionary dictionaryWithObjectsAndKeys:
+									font, NSFontAttributeName, nil] ;
+		answer = [self sizeForWidth:width
+							 height:height
+						 attributes:attributes] ;
+	}
+    
+	return answer ;
+}
+
+- (float)heightForWidth:(float)width
+				   font:(NSFont*)font {
+	return [self sizeForWidth:width
+					   height:FLT_MAX
+						 font:font].height ;
+}
+
+- (float)widthForHeight:(float)height
+				   font:(NSFont*)font {
+	return [self sizeForWidth:FLT_MAX
+					   height:height
+						 font:font].width ;
+}
+
+*/
 
 
 //--------------------------------------------------------------
